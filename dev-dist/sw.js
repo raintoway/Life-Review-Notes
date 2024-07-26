ee7c5f06876ba341f177909682399c63137459dc
caojiafu@cvte.com <caojiafu@cvte.com>
2024-07-26 14:00:08 +0800

    fix(all): 安全区域样式设置

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.k7f1hgoi9mo"
+    "revision": "0.vtqmnahh52o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
