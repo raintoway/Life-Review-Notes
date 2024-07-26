1405c22af5eef887ea3227c7337e294f3afa4bb7
caojiafu@cvte.com <caojiafu@cvte.com>
2024-07-26 20:35:13 +0800

    fix(all): 样式调整

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vtqmnahh52o"
+    "revision": "0.lfhce400sio"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
